eaa97f0b9d8009985aeff0039c8ba68acad0e042
Sunil <snilavarath@gmail.com>
2021-12-29 13:51:38 -0500

    commit tree bc51c78bd7f91f09a7ec7557f295361a6c831d32

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
+array = [2, 4, 6, 8, 10, 12]
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //iterate through array
+  for(let a= 0; a< array.length; a++){
+    //for each number, subtract from target and set the difference to a variable
+    let match = target-array[a]
+    //iterate through rest of array to find number that is equal to match variable
+    for (let b=a+1; b< array.length; b++){
+      if (array[b]===match) return true
+    }     
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  array variable with numbers inside
+  iterate through each number and subtract each number from target
+  if the difference is found in the array, return true
+  else, return false
 */
 
 /*
   Add written explanation of your solution here
+  first set an array with numbers inside 
+  function returns true if any pair of the numbers in the array return true when passed with the target number otherwise returns false 
+  hasTargetSum contains method to check sum of every possible pair of numbers
+  
 */
 
 // You can run `node index.js` to view these console logs
